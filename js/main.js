486a5862ff3ac83828362ee0aa818a5d72624e06
jonathan de Montalembert <demonj92@gmail.com>
2012-11-19 18:23:50 +0800

    added sliding effect

--- js/main.js
+++ js/main.js
@@ -1,4 +1,73 @@
-$('.iosSlider').iosSlider();
+
+  $('.iosSlider.banner').iosSlider({
+    desktopClickDrag: true,
+    snapToChildren: true,
+    infiniteSlider: true,
+    navSlideSelector: '.sliderContainer .slideSelectors .item',
+    onSlideComplete: slideComplete,
+    onSliderLoaded: sliderLoaded,
+    onSlideChange: slideChange,
+    autoSlide: true,
+    scrollbar: true,
+    scrollbarContainer: '.sliderContainer .scrollbarContainer',
+    scrollbarMargin: '0',
+    scrollbarBorderRadius: '0',
+    keyboardControls: true
+  });
+  
+  // prettyPrint();
+
+
+
+function slideChange(args) {
+      
+  $('.sliderContainer .slideSelectors .item').removeClass('selected');
+  $('.sliderContainer .slideSelectors .item:eq(' + (args.currentSlideNumber - 1) + ')').addClass('selected');
+
+}
+
+function slideComplete(args) {
+    
+  $(args.sliderObject).find('.text1, .text2').attr('style', '');
+  console.log($(args.sliderObject).find('.text1, .text2'))
+  $(args.currentSlideObject).find('.text1').animate({
+    right: '100px',
+    opacity: '0.8'
+  }, 400, 'easeOutQuint');
+  
+  $(args.currentSlideObject).find('.text2').delay(200).animate({
+    right: '50px',
+    opacity: '0.8'
+  }, 400, 'easeOutQuint');
+  
+}
+
+function sliderLoaded(args) {
+    
+  slideComplete(args);
+  
+  slideChange(args);
+  
+}
+
+$('.iosSlider.row1').iosSlider({
+    desktopClickDrag: true,
+    snapToChildren: true,
+    infiniteSlider: true,
+    navSlideSelector: '.sliderContainer .slideSelectors .item',
+    onSlideComplete: slideComplete,
+    onSliderLoaded: sliderLoaded,
+    onSlideChange: slideChange,
+    scrollbar: true,
+    scrollbarContainer: '.sliderContainer .scrollbarContainer',
+    scrollbarMargin: '0',
+    scrollbarBorderRadius: '0',
+    keyboardControls: true
+  });
+
+// end slide conf
+ 
+
 
 jQuery.fn.center = function(parent) {
     if (parent) {
@@ -14,7 +83,7 @@ jQuery.fn.center = function(parent) {
 return this;
 }
 
-
+MBP.hideUrlBarOnLoad();
 $('.hasModal').on('click', function(event){openPopup($(this))});
 $('.hasModal').on('touchstart', function(event){openPopup($(this))});
 var openPopup = function(that){
@@ -53,8 +122,8 @@ $('.close').on('click', function(){Avgrund.hide()});
 // Adjust the  on resising
 
 
-$(document).ready(function(){
-	$('body').queryLoader2({
+window.addEventListener('DOMContentLoaded', function() {
+    $("body").queryLoader2({
         barColor: "#ffffff",
         backgroundColor: "#000000",
         percentage: false,
@@ -62,8 +131,10 @@ $(document).ready(function(){
         completeAnimation: "grow",
         minimumTime: 100
     });
-    $('body').css({'display':'block'})
+    $('body').removeClass('invisible')
 });
+
+        
 // end on dom ready
 
 // help for device sizing, erase once dev finish
